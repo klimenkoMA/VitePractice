fe444bda590120f8cddd4479d98deb33282b73e2
klimenko.ma.1982@gmail.com <klimenko.ma.1982@gmail.com>
2024-03-28 16:31:42 +0300

    Added ChatBot.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import YandexAIComponent from "./components/YandexAIComponent";
+import ChatBotComponent from "./chatbot/ChatBot";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -21,8 +23,11 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-
       </div>
+        <div>
+            {/*<YandexAIComponent/>*/}
+            <ChatBotComponent/>
+        </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -3,5 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  "proxy": {
+    "/api/yandexAI": {
+      "target": "https://api.ai.yandex.net",
+      "changeOrigin": true,
+      "pathRewrite": {
+        "^/api/yandexAI": ""
+      }
+    }
+  },
   plugins: [react()],
+    optimizeDeps: {
+        exclude: ['chun']
+    }
 })
