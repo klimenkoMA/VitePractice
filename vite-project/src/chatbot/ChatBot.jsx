1dc38f306b22d83e127c546ccebd77982c6982c8
Mikl Klimenko <klimenko.ma.1982@gmail.com>
2024-03-29 06:16:23 +0300

    Added cosmetic changes

--- vite-project/src/chatbot/ChatBot.jsx
+++ vite-project/src/chatbot/ChatBot.jsx
@@ -1,30 +1,30 @@
-import React, { useState } from 'react';
-import { Widget } from 'react-chatbot-widget';
-
-const ChatBotComponent = () => {
-    const [opened, setOpened] = useState(false);
-
-    const handleOpen = () => {
-        setOpened(true);
-    };
-
-    const handleClose = () => {
-        setOpened(false);
-    };
-
-    return (
-        <div>
-            <button onClick={handleOpen}>Open Chat Bot</button>
-            {opened && (
-                <Widget
-                    handleNewUserMessage={(newMessage) => console.log(`New message incoming! ${newMessage}`)}
-                    title="ChatBot"
-                    subtitle="Ask me anything!"
-                    senderPlaceHolder="Type a message..."
-                />
-            )}
-        </div>
-    );
-};
-
-export default ChatBotComponent;
+// import React, { useState } from 'react';
+// import { Widget } from 'react-chatbot-widget';
+//
+// const ChatBotComponent = () => {
+//     const [opened, setOpened] = useState(false);
+//
+//     const handleOpen = () => {
+//         setOpened(true);
+//     };
+//
+//     const handleClose = () => {
+//         setOpened(false);
+//     };
+//
+//     return (
+//         <div>
+//             <button onClick={handleOpen}>Open Chat Bot</button>
+//             {opened && (
+//                 <Widget
+//                     handleNewUserMessage={(newMessage) => console.log(`New message incoming! ${newMessage}`)}
+//                     title="ChatBot"
+//                     subtitle="Ask me anything!"
+//                     senderPlaceHolder="Type a message..."
+//                 />
+//             )}
+//         </div>
+//     );
+// };
+//
+// export default ChatBotComponent;
