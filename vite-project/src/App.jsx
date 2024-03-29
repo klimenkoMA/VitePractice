3193a7b5a63b7dfdd279a1725609a1b505ad739d
klimenko.ma.1982@gmail.com <klimenko.ma.1982@gmail.com>
2024-03-29 15:37:26 +0300

    Added chatbot`s dialog form in ChatBot.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ChatBotComponent from "./chatbot/ChatBot";
+import "./styles/ChatBotFormStyle.css";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -24,7 +26,7 @@ function App() {
       </div>
         <div>
             {/*<YandexAIComponent/>*/}
-            {/*<ChatBotComponent/>*/}
+            <ChatBotComponent/>
         </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
--- vite-project/src/chatbot/ChatBot.jsx
+++ vite-project/src/chatbot/ChatBot.jsx
@@ -1,30 +1,50 @@
-// import React, { useState } from 'react';
-// import { Widget } from 'react-chatbot-widget';
-//
-// const ChatBotComponent = () => {
-//     const [opened, setOpened] = useState(false);
-//
-//     const handleOpen = () => {
-//         setOpened(true);
-//     };
-//
-//     const handleClose = () => {
-//         setOpened(false);
-//     };
-//
-//     return (
-//         <div>
-//             <button onClick={handleOpen}>Open Chat Bot</button>
-//             {opened && (
-//                 <Widget
-//                     handleNewUserMessage={(newMessage) => console.log(`New message incoming! ${newMessage}`)}
-//                     title="ChatBot"
-//                     subtitle="Ask me anything!"
-//                     senderPlaceHolder="Type a message..."
-//                 />
-//             )}
-//         </div>
-//     );
-// };
-//
-// export default ChatBotComponent;
+import React, {useState} from 'react';
+import "../styles/ChatBotFormStyle.css";
+
+const ChatBotComponent = () => {
+    const [question, setQuestion] = useState('');
+    const [answer, setAnswer] = useState('');
+    const [inputValue, setInputValue] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Действия по обработке данных из поля ввода
+
+        // Очистка поля ввода
+        setInputValue('');
+    };
+
+    const handleQuestionChange = (e) => {
+        setQuestion(e.target.value);
+    };
+
+    const handleQuestionSubmit = () => {
+        // Здесь можно добавить логику для получения ответа на вопрос пользователя
+        // Например, можно использовать API для обработки запроса к чатботу
+        setAnswer(`Ответ на вопрос "${question}"`);
+    };
+
+    return (
+        <div className="dialogue-container">
+            <h4>Информационная система</h4>
+            <div className="bot-message">{answer}</div>
+            <div className="message-form">
+                <input
+                    className="message-input"
+                    type="text"
+                    placeholder="Введите ваш вопрос"
+                    value={question}
+                    onChange={handleQuestionChange}
+                    onBlur={handleSubmit}
+                />
+                <button className="send-button"
+                        onClick={handleQuestionSubmit}
+                >
+                    Отправить
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default ChatBotComponent;
